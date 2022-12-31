a862e957946284d7693aa3457c21b2f2eb69eac6
NickWils98 <nick.wils@student.uantwerpen.be>
2022-12-31 03:18:24 +0100

    cleanup control_tower.py

--- model/atomic_devs/control_tower.py
+++ model/atomic_devs/control_tower.py
@@ -4,11 +4,15 @@ from messages_events.port_events import portEntryPermission
 # Define the state of the Control Tower as a structured object
 class ControlTowerState:
     def __init__(self, dock_count=8):
+        # list for responses
         self.answers = []
+        # list of all the docks with their places available
         self.docks = []
         for _ in range(dock_count):
             self.docks.append(50)
+        # list of request in FIFO
         self.queue = []
+        # there is a place free in a dock
         self.place_free = True
 
 class ControlTower(AtomicDEVS):
@@ -22,17 +26,21 @@ class ControlTower(AtomicDEVS):
         self.free_event = self.addInPort("free_event")
 
     def intTransition(self):
+        #  if there is a request pending and place in a dock
         if len(self.state.queue) > 0 and self.state.place_free == True:
             request = self.state.queue.pop(0)
             quay_id = 0
+            # find a free dock
             for i in range(0, len(self.state.docks)):
                 if self.state.docks[i] >= 1:
                     self.state.docks[i] -= 1
                     quay_id = i + 1
                     break
+            # if no dock was free put it back first
             if quay_id == 0:
                 self.state.queue.insert(0,request)
                 self.state.place_free = False
+            # make a response
             else:
                 answer = portEntryPermission(request.vessel_id, quay_id, request.current_time)
                 self.state.answers.append(answer)
@@ -42,6 +50,7 @@ class ControlTower(AtomicDEVS):
         # Check if there are ships who sent message they want to enter the dock
         if self.in_event in inputs:
             for request in inputs[self.in_event]:
+                #  check if there is an availability
                 quay_id = 0
                 for i in range(0, len(self.state.docks)):
                     if self.state.docks[i]>=1:
@@ -49,9 +58,11 @@ class ControlTower(AtomicDEVS):
                         quay_id = i+1
                         break
                 if quay_id == 0:
+                    # if not put it in the queue
                     self.state.queue.append(request)
                     self.state.place_free = False
                 else:
+                    #  if there is make a response
                     answer = portEntryPermission(request.vessel_id, quay_id, request.current_time)
                     self.state.answers.append(answer)
 
@@ -74,9 +85,7 @@ class ControlTower(AtomicDEVS):
     def outputFnc(self):
         return_dict = {}
         if len(self.state.answers) > 0:
-
             answer = self.state.answers
-
             return_dict[self.out_event] = answer
             self.state.answers = []
         return return_dict
